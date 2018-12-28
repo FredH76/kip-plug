1afdcad477bac3ec1b02c4c9d197daaec9561951
Frederic Huguenin <fred76temp@gmail.com>
2018-12-28 14:36:50 +0100

    feat: add 3 buttons + timer + onResumeSubscription

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,50 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CallNumber } from '@ionic-native/call-number';
+import { Platform } from 'ionic-angular';
+import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs/Observable.js'
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public count: number = 0;
+  private onTimerSubscription: Subscription;
+  private onResumeSubscription: Subscription;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private callNumber: CallNumber,
+    platform: Platform) {
+    this.onResumeSubscription = platform.resume.subscribe(() => {
+      // do something meaningful when the app is put in the foreground
+      console.error("RESUME APP");
+    });
 
+  }
+
+  public startTimer() {
+    this.count = 0;
+    this.onTimerSubscription = Observable.interval(1000).subscribe(x => {
+      this.count++;
+    })
+  }
+
+  public startPhoneCall() {
+    this.callNumber.callNumber("0664545968", true);
+  }
+
+  public stopTimer() {
+    this.onTimerSubscription.unsubscribe();
+  }
+
+  ngOnDestroy() {
+    // always unsubscribe your subscriptions to prevent leaks
+    this.onTimerSubscription.unsubscribe();
+    this.onResumeSubscription.unsubscribe();
   }
 
 }
